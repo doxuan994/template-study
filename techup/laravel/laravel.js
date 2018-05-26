3c9fb922b91e41b8a474b0fe1ab155fcd367c4eb
Xuan Do <doxuancntp@gmail.com>
2018-05-26 17:53:17 -0400

    Initial Commit

---
+++ techup/laravel/laravel.js
@@ -0,0 +1,48 @@
+$('#sidebar').affix({
+    offset: {
+        top: 245
+    }
+});
+
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+    target: '#leftCol',
+    offset: navHeight
+});
+
+// sandbox disable popups
+if (window.self !== window.top && window.name!="view1") {;
+    window.alert = function(){/*disable alert*/};
+    window.confirm = function(){/*disable confirm*/};
+    window.prompt = function(){/*disable prompt*/};
+    window.open = function(){/*disable open*/};
+}
+
+// prevent href=# click jump
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.getElementsByTagName("A");
+    for(var i=0; i < links.length; i++) {
+        if(links[i].href.indexOf('#')!=-1) {
+            links[i].addEventListener("click", function(e) {
+                console.debug("prevent href=# click");
+                if (this.hash) {
+                    if (this.hash=="#") {
+                        e.preventDefault();
+                        return false;
+                    }
+                    else {
+                        /*
+                        var el = document.getElementById(this.hash.replace(/#/, ""));
+                        if (el) {
+                        el.scrollIntoView(true);
+                    }
+                    */
+                }
+            }
+            return false;
+        })
+    }
+}
+}, false);
